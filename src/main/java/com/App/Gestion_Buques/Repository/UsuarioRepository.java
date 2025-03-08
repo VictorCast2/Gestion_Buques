74578e875c69a3ffcf445ccc73f7ca94b7cbf408
TheMidnigth <reyeskeyner110@gmail.com>
2025-03-08 17:27:59 -0500

    clase usuarioRepository corrigida victor ya no uses mas chat gtp bro eso no es de dios

--- src/main/java/com/App/Gestion_Buques/Repository/UsuarioRepository.java
+++ src/main/java/com/App/Gestion_Buques/Repository/UsuarioRepository.java
@@ -2,12 +2,11 @@ package com.App.Gestion_Buques.Repository;
 
 import com.App.Gestion_Buques.Entity.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long>, CrudRepository<UsuarioEntity, Long> {
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
     // Método para buscar un usuario por su nombre de usuario
     Optional<UsuarioEntity> findByUsername(String username);
     // Método para buscar un usuario por su email
