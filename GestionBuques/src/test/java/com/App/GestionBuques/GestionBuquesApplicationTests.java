6d3adaea275fd6fdd45f442bdde1f64d64b4c593
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-03-12 10:29:16 -0500

    Creación del Nuevo Proyecto

---
+++ GestionBuques/src/test/java/com/App/GestionBuques/GestionBuquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.App.GestionBuques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBuquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
