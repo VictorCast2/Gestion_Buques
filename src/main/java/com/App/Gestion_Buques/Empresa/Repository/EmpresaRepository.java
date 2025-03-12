5d05df8c91a91131993af117439fad272dfd2ef6
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-03-12 10:23:47 -0500

    Update Main no funciono

--- src/main/java/com/App/Gestion_Buques/Empresa/Repository/EmpresaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.App.Gestion_Buques.Empresa.Repository;
-
-import org.springframework.stereotype.Repository;
-import com.App.Gestion_Buques.Empresa.Entity.EmpresaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface EmpresaRepository extends JpaRepository<EmpresaEntity, Long>{
-}
