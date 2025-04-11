c348c615c7227b3261c162db01fa45e5a2b134e2
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-04-11 00:27:10 -0500

    Quitar el JWT de Empresa

--- src/main/java/com/example/buques/dto/response/EmpresaResponse.java
+++ src/main/java/com/example/buques/dto/response/EmpresaResponse.java
@@ -3,7 +3,7 @@ package com.example.buques.dto.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"nit", "nombre", "pais", "ciudad", "direccion", "telefono", "correo", "jwt", "cantidad_buques"})
+@JsonPropertyOrder({"nit", "nombre", "pais", "ciudad", "direccion", "telefono", "correo", "cantidad_buques"})
 public record EmpresaResponse(
         @JsonProperty("nit") String nit,
         @JsonProperty("nombre") String nombre,
@@ -12,7 +12,6 @@ public record EmpresaResponse(
         @JsonProperty("direccion") String direccion,
         @JsonProperty("telefono") String telefono,
         @JsonProperty("correo") String correo,
-        @JsonProperty("jwt") String jwt,
         @JsonProperty("cantidad_buques") int cantidad_buques
         ) {
 }
