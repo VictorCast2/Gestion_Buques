39629806187500fa69171864c57c8628e8ef9f1a
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-04-18 17:01:17 -0500

    Actualización de la Empresa Controller

---
+++ src/main/java/com/app/controller/EmpresaController.java
@@ -0,0 +1,30 @@
+package com.app.controller;
+
+import com.app.dto.request.EmpresaRequest;
+import com.app.dto.response.EmpresaResponse;
+import com.app.service.EmpresaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/empresa")
+public class EmpresaController {
+
+    @Autowired
+    private EmpresaService empresaService;
+
+    @GetMapping("/registro")
+    public String registro(Model model) {
+        return "Empresa";
+    }
+
+    @PostMapping("/registrar")
+    public String registrarEmpresa(@Valid @RequestBody EmpresaRequest request, String correoUsuario) {
+        EmpresaResponse response = empresaService.registrarEmpresaAUsuario(request, correoUsuario);
+        return "Empresa";
+    }
+
+}
--- src/main/java/com/app/service/EmpresaService.java
+++ src/main/java/com/app/service/EmpresaService.java
@@ -6,23 +6,20 @@ import com.app.collections.Usuario.pojo.Empresa;
 import com.app.dto.request.EmpresaRequest;
 import com.app.dto.response.EmpresaResponse;
 import com.app.repository.UsuarioRepository;
-import com.app.utils.JwtUtils;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.validation.Valid;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Data
 @Service
 public class EmpresaService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    @Autowired
-    private JwtUtils jwtUtils;
-
-    public EmpresaResponse asociarEmpresaAUsuario(@Valid EmpresaRequest request, String correoUsuario) {
+    public EmpresaResponse registrarEmpresaAUsuario(@Valid EmpresaRequest request, String correoUsuario) {
 
         Usuario usuario = usuarioRepository.findByCorreo(correoUsuario)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con el correo: " + correoUsuario));
