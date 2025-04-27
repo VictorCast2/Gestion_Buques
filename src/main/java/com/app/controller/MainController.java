a267ecb8b829b75a6b0fe24cc080af542599dc1c
J0SE <joseandrestorres0508@gmail.com>
2025-04-26 23:35:19 -0500

    refactor: Optimizar el método "actualizar contraseña" para que se muestren correctamente los mensajes en el front

--- src/main/java/com/app/controller/MainController.java
+++ src/main/java/com/app/controller/MainController.java
@@ -5,17 +5,13 @@ import com.app.dto.request.UpdatePasswordRequest;
 import com.app.dto.response.AuthResponse;
 import com.app.security.CustomUserDetails;
 import com.app.service.UserDetailServiceImpl;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriUtils;
 
 import java.nio.charset.StandardCharsets;
