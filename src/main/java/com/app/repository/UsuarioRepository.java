1063a4ab8b607c3d30bb098959ceb4e8f2e94aee
J0SE <joseandrestorres0508@gmail.com>
2025-05-05 11:27:16 -0500

    feat: Agregar método para crear ua factura/proceso a un agente naviero (falta Optimizar)

--- src/main/java/com/app/repository/UsuarioRepository.java
+++ src/main/java/com/app/repository/UsuarioRepository.java
@@ -4,9 +4,12 @@ import com.app.collections.Usuario.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface UsuarioRepository extends MongoRepository<Usuario, String> {
     Optional<Usuario> findByCorreo(String correo);
     boolean existsByCorreo(String correo);
+
+    Set<Usuario> findAllByEmpresa_Nit(String nit);
 }
