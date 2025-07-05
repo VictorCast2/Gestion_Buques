b5cac2fb494f8cf3da70efd6fb8f178117cd67f9
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-07-05 12:01:02 -0500

    Actualizar el Spring y el Angular

---
+++ GestionBuques - Spring/src/main/java/com/Proyect/GestionBuques/GestionBuques/GestionBuquesApplication.java
@@ -0,0 +1,13 @@
+package com.Proyect.GestionBuques.GestionBuques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBuquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBuquesApplication.class, args);
+	}
+
+}
---
+++ GestionBuques - Spring/src/test/java/com/Proyect/GestionBuques/GestionBuques/GestionBuquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyect.GestionBuques.GestionBuques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBuquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
