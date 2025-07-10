3ca983dfb82a35efe159c8501e52f3bc6535cb5d
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-07-09 19:26:00 -0500

    Actualización del Spring Boot

--- GestionBuques-Spring/src/main/java/com/Proyect/GestionBuques/GestionBuques/GestionBuquesApplication.java
+++ GestionBuques-Spring/src/main/java/com/Proyect/GestionBuques/GestionBuques/GestionBuquesApplication.java
@@ -1,7 +1,9 @@
 package com.Proyect.GestionBuques.GestionBuques;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GestionBuquesApplication {
@@ -9,5 +11,10 @@ public class GestionBuquesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestionBuquesApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner init() {
+        return null;
+    }
 
 }
